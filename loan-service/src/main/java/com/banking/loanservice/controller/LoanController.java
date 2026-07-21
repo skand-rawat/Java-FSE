58d45ee1734adef5068e121a66fb07d4e1901a4d
Skand Rawat <skandrawat0@gmail.com>
2026-07-21 17:58:42 +0530

    Initial import of week4 project

---
+++ loan-service/src/main/java/com/banking/loanservice/controller/LoanController.java
@@ -0,0 +1,15 @@
+package com.banking.loanservice.controller;
+
+import com.banking.loanservice.model.Loan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loans/{number}")
+    public Loan getLoan(@PathVariable String number) {
+        return new Loan(number, "car", 400000, 3258, 18);
+    }
+}
