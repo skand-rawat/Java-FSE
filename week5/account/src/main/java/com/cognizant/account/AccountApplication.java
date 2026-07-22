ce0c340c658844c8c0bc6658ad92ff13ec8bb95f
skand-rawat <skand.rawat@example.com>
2026-07-22 18:22:17 +0530

    Restore missing Week 5 account and eureka-discovery-server folders

---
+++ week5/account/src/main/java/com/cognizant/account/AccountApplication.java
@@ -0,0 +1,15 @@
+package com.cognizant.account;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication
+public class AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+
+}
+
+
